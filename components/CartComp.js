752c0bbedfdae93165bae7cd4f8b7deac9217258
AbdeldjabarDev <ousseddikabdo@gmail.com>
2022-12-23 20:06:47 +0100

    Fixed NextJS issues

---
+++ components/CartComp.js
@@ -0,0 +1,93 @@
+import store from "../state/store";
+import Nav from "../components/Nav";
+import CartProductCont from "../components/CartProductCont";
+import { useEffect, useState } from "react";
+import { Router, useRouter } from "next/router";
+import { getCookie } from "cookies-next";
+export default function CartComp(props) {
+  let router = useRouter();
+  let [empty, setEmpty] = useState(false);
+  let [loading,setLoading] = useState(false);
+  let [totalAmount,setTotalAmount] = useState(store.getState().cart.total);
+  useEffect(() => {
+    if (store.getState().cart.value.length == 0) setEmpty(true);
+    return store.subscribe(() => {
+      if (store.getState().cart.value.length == 0) setEmpty(true);
+      
+      setTotalAmount(store.getState().cart.total);
+    
+    });
+  });
+  
+  if (empty)
+    return (
+      <div className="w-full h-[100vh] flex flex-col">
+        <Nav></Nav>
+        <div className="w-[100%] lg:mt-[15%] lg:w-[50%] bg-white ml-auto mr-auto  border-black rounded-md h-[50vh] flex shadow-lg mt-[50%]">
+          <div className="ml-auto mr-auto mt-[23%] dark:text-black text-xl">
+            You have no items in your cart
+          </div>
+        </div>
+      </div>
+    );
+  return (
+    <div className="w-full h-full flex flex-col">
+      <Nav></Nav>
+      <div className="lg:w-[50%] w-full ml-auto mr-auto border-none lg:border border-black  rounded-md shadow-none lg:shadow-lg flex flex-col mt-[25%] lg:mt-[10%] gap-4">
+        <div className="flex flex-col ml-0 lg:ml-[10%]">
+          {store.getState().cart.value.map((e, i) => {
+            return <CartProductCont product={e} count={i} key={e.id}/>;
+          })}
+        </div>
+
+        <div className="flex flex-col  border-red-400 ">
+        <div className="ml-auto mr-[10%] mb-[2%]">{"Total : " +totalAmount+ "$"}</div>
+          <button
+            className="bg-green-600 p-3 rounded-md mb-10 font-semibold ml-auto mr-auto text-white"
+            onClick={(e) => {
+              setLoading(true);
+              let items = [];
+              let current_products = store.getState().cart.value;
+              for (let i = 0; i < current_products.length; i++) {
+                items.push({
+                  price_data: {
+                    currency: "usd",
+                    product_data: {
+                      name: current_products.at(i).title,
+                    },
+                    unit_amount: current_products.at(i).price*100,//for stripe 
+                  },
+                  quantity: store.getState().cart.count.at(i),
+                });
+              }
+             // console.log(JSON.stringify({its:items,uid:store.getState().cart.user}));
+              fetch("http://localhost:23000/purchase", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({its:items,uid:getCookie('topshop_userId')}),
+              })
+              .then((response)=> response.json())
+              .then((data)=>
+              {
+                setLoading(false);
+                if(data.url!=undefined)
+                router.push(data.url)
+
+              })
+              .catch((err) => console.log(err));
+            }}
+          >
+            
+            {loading == false ? "Confirm" : ""}
+            <div
+              className="w-8 h-8 ml-auto mr-auto rounded-full border border-blue-900 border-b-white animate-spin"
+              style={{ display: loading == true ? "block" : "none" }}
+            ></div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/ResetComp.js
@@ -0,0 +1,165 @@
+import { useRouter } from "next/router";
+import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import Carouseler from "../components/Carouseler";
+import {setResetEmail,removeResetEmail,setResetCode,removeResetCode} from '../state/cartSlice'
+import store from "../state/store";
+function RequestReset(props)
+{
+    
+}
+export default function ResetComp(props)
+{
+    
+    let contRef = useRef();
+   let router = useRouter();
+   let eRef = useRef();
+   let cRef = useRef();
+   let pRef = useRef();
+   let npRef = useRef();
+   let [error,setE] = useState();
+   let [loading,setLoading] = useState(false);
+   let dispatch = useDispatch();
+   
+    return(<div className="w-full h-[100%] border-[3px] border-green-600 bg-black flex flex-col">
+      <div className="w-[90%]  bg-white  border  border-red-600 overflow-hidden ml-auto mr-auto mt-[10%]">
+      <div className="flex" style={{width:"400%"}} ref={contRef}>
+     <div className="w-full h-full  flex flex-col gap-6 mt-[2%]">
+      <div className="ml-auto mr-auto dark:text-black text-center">Enter your email so that we can send you a verification code</div>
+      <input className="ml-auto mr-auto lg:w-[70%] w-[90%] pl-2 h-[6vh] rounded-md border" placeholder="Email" ref={eRef}></input>
+      <button className="p-3 rounded-md shadow-md bg-green-600 text-white ml-auto mr-auto" onClick={(e)=>
+      {
+        console.log('Email submitted ')
+        dispatch(setResetEmail(eRef.current.value.toString()));
+        // if(em.match(/(a-zA-Z0-9)*@([a-z])+\.([a-z])+/g) ==  null )
+        // {
+        //   setE('Please enter a valid email');
+        //   return;
+        // } 
+        fetch(process.env.BACKEND_URL  || "http://localhost:23000" +'/reset',{
+          method:'POST',
+          body:JSON.stringify({email:store.getState().cart.resetEmail}),
+          headers:{
+            'Content-Type':'application/json'
+          }
+        }).then((r) => r.json())
+          .then((data)=>
+          {
+            setLoading(false)
+            if(data.error == 0)
+            {
+              contRef.current.style.transform = "translateX(-25%)"
+            }
+            if(data.error == 5)
+            {
+              setE("Internal service error please try again later")
+            }
+          }).catch((e)=>
+          {
+            console.log(e);
+            setLoading(false)
+          })
+          setLoading(true);
+        
+      }}>{loading == true ? "":"Proceed"} <div
+      className="w-8 h-8 ml-auto mr-auto rounded-full border border-white border-b-blue-500 animate-spin"
+      style={{ display: loading == true ? "block" : "none" }}
+    ></div></button>
+      <div className="text-red-600">{error}</div>
+     </div>
+     <div className="w-full h-full flex flex-col gap-4 s mt-[2%]">
+      <div className="ml-auto mr-auto">We sent a code to {store.getState().cart.resetEmail.substring(0,3)+"****"+store.getState().cart.resetEmail.substring(store.getState().cart.resetEmail.indexOf('@'),store.getState().cart.resetEmail.length)}</div>
+      <input className="ml-auto mr-auto w-[70%] pl-2 h-[6vh] rounded-md border" placeholder="Verification code" ref={cRef}></input>
+      <button className="p-3 rounded-md shadow-md bg-green-600 text-white ml-auto mr-auto" onClick={(e)=>
+      {
+        dispatch(setResetCode(cRef.current.value.toString()));
+        fetch(process.env.BACKEND_URL  || "http://localhost:23000"+'/reset/verify',{
+          method:'POST',
+          body:JSON.stringify({email:store.getState().cart.resetEmail,code:store.getState().cart.resetCode}),
+          headers:{
+            'Content-Type':'application/json'
+          }
+        }).then((r) => r.json())
+          .then((data)=>
+          {
+            setLoading(false);
+            if(data.error == 0)
+            {
+
+              contRef.current.style.transform = "translateX(-50%)"
+            }
+            else
+            {
+              setE(data.message)
+            }
+          }).catch((e)=>
+          {
+            setLoading(false);
+            console.log(e);
+            
+          })
+          setLoading(true);
+      }}>{loading == true ? "":"Proceed"} <div
+      className="w-8 h-8 ml-auto mr-auto rounded-full border border-white border-b-blue-500 animate-spin"
+      style={{ display: loading == true ? "block" : "none" }}
+    ></div></button>
+      <div className="text-red-600">{error}</div>
+      </div>
+      <div className="w-full h-full flex flex-col mt-[2%] pl-6 gap-5">
+       <div>New password</div>
+       <input className=" w-[70%] pl-2 h-[6vh] rounded-md border" placeholder="Verification code" ref={pRef}></input>
+      <div>Confirm new password</div>
+      <input className=" w-[70%] pl-2 h-[6vh] rounded-md border" placeholder="Verification code" ref={npRef}></input>
+      <button className="p-3 rounded-md shadow-md bg-green-600 text-white ml-auto mr-auto" onClick={(e)=>
+      {
+        if(npRef.current.value !== pRef.current.value)
+        {
+          setE('Passwords do not match');
+          return;
+        }
+        else if(pRef.current.value == '')
+        {
+          setE('Password field cannot be empty');
+          return;
+        }
+       
+        fetch(process.env.BACKEND_URL || "http://localhost:23000" + '/reset/new',{
+          method:'POST',
+          body:JSON.stringify({email:store.getState().cart.resetEmail,code:store.getState().cart.resetCode,password_hash:pRef.current.value}),
+          headers:{
+            'Content-Type':'application/json'
+          }
+        }).then((r) => r.json())
+          .then((data)=>
+          {
+            setLoading(false)
+            if(data.error == 0)
+            {
+              contRef.current.style.transform = "translateX(-75%)"
+            }
+          }).catch((e)=>
+          {
+            console.log(e)
+            setLoading(false)
+          })
+          setLoading(true)
+      }}>{loading == true ? "":"Proceed"} <div
+      className="w-8 h-8 ml-auto mr-auto rounded-full border border-white border-b-blue-500 animate-spin"
+      style={{ display: loading == true ? "block" : "none" }}
+    ></div></button>
+      <div className="text-red-600">{error}</div>
+      </div>
+      <div className="w-full h-full flex flex-col mt-[2%]">
+<div>Password has been reset successfully </div>
+<button className="p-3 rounded-md shadow-md bg-green-600 text-white ml-auto mr-auto" onClick={(e)=>
+      {
+        router.replace('/login')
+      }}>Continue</button>
+      </div>
+      </div>
+      </div>
+  
+     
+     
+    </div>)
+}
