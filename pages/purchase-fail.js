5d03a99ad24f485086ef8bfbc9fe9f66ab8e44ea
AbdeldjabarDev <ousseddikabdo@gmail.com>
2023-03-08 13:42:51 +0100

    Fixed build issue

--- pages/purchase-fail.js
+++ pages/purchase-fail.js
@@ -7,7 +7,7 @@ export default function PurchaseFail()
     let dark = useSelector((state)=> state.products.dark);
     return (<div className="flex flex-col w-full h-full" style={{backgroundColor : dark == true ? "#000d2f":"white",color:dark == true ? "white":"black"}}>
 <Nav></Nav>
-<div className="w-[40%] h-[40vh] flex flex-col mt-[15vh] shadow-md bg-white ml-auto mr-auto border ">
+<div className="w-[40%] h-[40vh] flex flex-col mt-[15vh] shadow-md bg-white ml-auto mr-auto border " style={{backgroundColor : dark == true ? "#000d1a":"white",color:dark == true ? "white":"black"}}>
 <div className="ml-auto mr-auto font-bold text-2xl mt-[5%]">Sorry</div>
 <div className="ml-auto mr-auto">Seems like your purchase did not succeed.Please try again !</div>
 <div className="ml-auto mr-auto">If the issue persists please contact us </div>
--- pages/purchase-sucess.js
+++ pages/purchase-sucess.js
@@ -1,12 +1,13 @@
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
-export default function PurchaseSuccss()
+import Nav from "../components/Nav";
+export default function PurchaseSuccess()
 {
     let router= useRouter();
     let dark = useSelector((state)=> state.products.dark);
-    return (<div className="flex flex-col w-full min-h-screen h-fit" style={{backgroundColor : dark == true ? "#000d1a":"white",color:dark == true ? "white":"black"}}>
+    return (<div className="flex flex-col w-full min-h-screen h-fit" style={{backgroundColor : dark == true ? "#000d2f":"white",color:dark == true ? "white":"black"}}>
     <Nav></Nav>
-    <div className="w-[40%] h-[40vh] flex flex-col mt-[15vh] shadow-md bg-white ml-auto mr-auto border ">
+    <div className="w-[40%] h-[40vh] flex flex-col mt-[15vh] shadow-md bg-white ml-auto mr-auto border " style={{backgroundColor : dark == true ? "#000d1a":"white",color:dark == true ? "white":"black"}}>
     <div className="ml-auto mr-auto font-bold text-2xl mt-[5%]">Great !</div>
     <div className="ml-auto mr-auto">Your items are on their way to you .</div>
     <div className="ml-auto mr-auto">You can check their status in the "My Purchases" Section on the side nav.</div>
